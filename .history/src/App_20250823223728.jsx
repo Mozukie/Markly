ef67d84a67925f1152c2e43e71fe9360c0c3c88c
Pauline <Pauline>
2025-08-24 19:56:28 +0800

    update huhu

---
+++ .history/src/App_20250823223728.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'React-Router-Dom'
+import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import { AnimatePresence, motion } from 'framer-motion'
+import NotFound from './pages/NotFound.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import About from './components/About.jsx'
+import StudentDashboard from './pages/Students/studentdashboard.jsx'
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.4 }}
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home/>} />
+          <Route path="*" element={<NotFound/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/about" element={<About/>} />          
+          <Route path="/studentdashboard" element={<StudentDashboard/>} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
+function App() {
+  return(
+    <div>
+      <Router>
+        <AnimatedRoutes />
+      </Router>
+    </div>
+  )
+}
+
+export default App
