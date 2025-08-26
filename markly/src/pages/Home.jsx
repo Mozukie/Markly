8db0300e651610fbcd9079e0da36c4650ca7425c
Pauline <bondocpauline06@gmail.com>
2025-08-26 20:36:33 +0800

    Added markly as a normal folder

---
+++ markly/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header.jsx";
+
+export default function Home() {
+    return (
+        <div>
+        <Header />
+        <main className="container mx-auto p-4">
+
+        </main>
+        </div>
+    );
+    }
