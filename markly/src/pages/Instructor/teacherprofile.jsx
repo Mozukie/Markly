98c6514a4e2df9996d3c24db22fc92887200b21d
Pauline <bondocpauline06@gmail.com>
2025-08-28 10:12:14 +0800

    revert

--- markly/src/pages/Instructor/teacherprofile.jsx
+++ markly/src/pages/Instructor/teacherprofile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Header from "../../components/Header.jsx";
-import TeacherModal from "../../components/teacherModal.jsx";
+import Header from "../components/Header.jsx";
+import TeacherModal from "../components/TeacherModal.jsx";
 
 export default function TeacherProfile() {
   const [teacher, setTeacher] = useState(null);
